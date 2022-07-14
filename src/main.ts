5533bbaf98482cb14b8086c43a42b62d18cb1e02
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-14 22:01:36 +0200

    defined the apollo to establish contact with the justwatch api.
    If applied, this commit will have defined through apollo the structure that will establish contact with the justwatch api.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "./assets/global.css";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).use(store).mount("#app");
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "https://apis.justwatch.com/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
+
+app.use(store).mount("#app");
