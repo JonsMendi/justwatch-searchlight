c76f39bf1bcb568c1a4cf567c1228e1e340d9275
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-17 22:39:04 +0200

    updated with new state and actions to organize the app
    If applied, this commit will have started to organize the app with the store that will contain the state, mutations, actions and gettters.
    
    Ideally everything will happen here and afterwords displayed in the component.
    
    At the moment is not working.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,68 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
+  state: () => ({
+    movies: [],
+  }),
+  getters: {},
   mutations: {
+    pushIntoMovies(state) {
+      state.movies.push();
+    },
   },
   actions: {
+    async getMovies(context) {
+      try {
+        const res = await axios({
+          method: "POST",
+          url: "https://apis.justwatch.com/graphql",
+          data: {
+            query: `
+    query GetSuggestedTitles(
+            $country: Country!
+            $language: Language!
+            $first: Int!
+            $filter: TitleFilter
+          ) {
+            popularTitles(country: $country, first: $first, filter: $filter) {
+              edges {
+                node {
+                  ...SuggestedTitle
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+          }
+          fragment SuggestedTitle on MovieOrShow {
+            id
+            objectType
+            objectId
+            content(country: $country, language: $language) {
+              fullPath
+              title
+              originalReleaseYear
+              posterUrl
+              fullPath
+              __typename
+            }
+            __typename
+          }`,
+            variables: {
+              country: "US",
+              language: "en",
+              first: 4,
+              filter: { searchQuery: "ola" },
+            },
+          },
+        });
+        context.commit("pushIntoMovies", res.data.Search);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
