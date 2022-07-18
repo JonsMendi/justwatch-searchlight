491b3307edb10758e88fd44c9c856b3b7bffe643
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-18 22:47:11 +0200

    updated with the actions working at last
    If applied, this commit will have initiated the communication in the store.
    
    It's already possible to fetch the movies from the api in actions and then update the state through the mutations.
    
    But still need to transfer all the logic from MovieComponent to the store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,63 +2,78 @@ import { createStore } from "vuex";
 import axios from "axios";
 
 export default createStore({
-  state: () => ({
-    movies: [],
-  }),
+  state: {
+    // Under was created moviesNew state to contain the information that "movies" state is containing
+    // in MovieComponent but still not working. (the name is suppose to be movies instead of moviesNew after making it work)
+    moviesNew: [] as any,
+    searchMovie: "",
+  },
   getters: {},
   mutations: {
-    pushIntoMovies(state) {
-      state.movies.push();
+    // Under pushIntoMovies should update the state moviesNew. Not working
+    setMovies: (state, payload) => {
+      state.moviesNew = payload;
+    },
+    searchMovie: (state, searchData) => {
+      console.log("search", searchData);
+      state.searchMovie = searchData;
     },
   },
   actions: {
-    async getMovies(context) {
+    // Under the async getMovies function should fetch the query and then we should be able to update the
+    // state moviesNew with a fetched data from getMovies. This need to happen with the help from mutation
+    // pushIntoMovies, but somehow it's not working still.
+    async getMovies({ commit }): Promise<void> {
+      const searchValue = commit("searchMovie");
       try {
-        const res = await axios({
+        const result = await axios({
           method: "POST",
           url: "https://apis.justwatch.com/graphql",
           data: {
             query: `
-    query GetSuggestedTitles(
-            $country: Country!
-            $language: Language!
-            $first: Int!
-            $filter: TitleFilter
-          ) {
-            popularTitles(country: $country, first: $first, filter: $filter) {
-              edges {
-                node {
-                  ...SuggestedTitle
-                  __typename
+              query GetSuggestedTitles(
+                  $country: Country!
+                  $language: Language!
+                  $first: Int!
+                  $filter: TitleFilter
+                ) {
+                  popularTitles(country: $country, first: $first, filter: $filter) {
+                    edges {
+                      node {
+                        ...SuggestedTitle
+                        __typename
+                      }
+                      __typename
+                    }
+                    __typename
+                  }
                 }
-                __typename
-              }
-              __typename
-            }
-          }
-          fragment SuggestedTitle on MovieOrShow {
-            id
-            objectType
-            objectId
-            content(country: $country, language: $language) {
-              fullPath
-              title
-              originalReleaseYear
-              posterUrl
-              fullPath
-              __typename
-            }
-            __typename
-          }`,
+                fragment SuggestedTitle on MovieOrShow {
+                  id
+                  objectType
+                  objectId
+                  content(country: $country, language: $language) {
+                    fullPath
+                    title
+                    originalReleaseYear
+                    posterUrl
+                    fullPath
+                    __typename
+                  }
+                  __typename
+                }`,
             variables: {
               country: "US",
               language: "en",
-              first: 4,
-              filter: { searchQuery: "ola" },
+              first: 5,
+              filter: { searchQuery: searchValue },
             },
           },
         });
-        context.commit("pushIntoMovies", res.data.Search);
+        // Under, it is still a lit bt confusing but at the moment is the way defined
+        // to extract different values from the returned data in order to be displayed.
+        commit("setMovies", result.data);
+        console.log(result.data);
       } catch (error) {
         console.log(error);
       }
