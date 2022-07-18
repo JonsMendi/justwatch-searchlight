951be093cfe48d0d882c0380c27afed6b70c7125
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-18 22:48:12 +0200

    created a testing component to setup the store communications.
    If applied, this commit will have created a testing component to setup the store communications.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="app">
     <MovieComponent />
+    <TestComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import MovieComponent from "./components/MovieComponent.vue";
+import TestComponent from "./components/TestComponent.vue";
 
 export default defineComponent({
   name: "JustWatch-SearchLight",
-  components: { MovieComponent },
+  components: { MovieComponent, TestComponent },
 });
 </script>
 
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <input type="text" />
+    <p>{{ moviesNew }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "vuex";
+
+export default defineComponent({
+  mounted() {
+    this.$store.dispatch("getMovies");
+    this.$store.dispatch("searchMovie");
+  },
+  computed: mapState(["moviesNew", "searchMovie"]),
+});
+</script>
+
+<style></style>
