bc0f6911aeff94c80438727a760a182032882a2a
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-18 22:43:17 +0200

    uninstalled apollo
    If applied, this commit will have uninstalled apollo because it's no longer in use.

--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,5 @@ import { createApp, h, provide } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "./assets/global.css";
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client/core";
-import { DefaultApolloClient } from "@vue/apollo-composable";
 
-// HTTP connection to the API
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: "https://apis.justwatch.com/graphql",
-});
-
-// Cache implementation
-const cache = new InMemoryCache();
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-});
-
-const app = createApp({
-  setup() {
-    provide(DefaultApolloClient, apolloClient);
-  },
-
-  render: () => h(App),
-});
-
-app.use(store).mount("#app");
+createApp(App).use(store).mount("#app");
